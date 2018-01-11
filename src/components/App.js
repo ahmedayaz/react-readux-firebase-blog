237aed7b6d55d0d478dfedea975a5475ff631cee
Ryan Dhungel <kaloraat@gmail.com>
2018-01-11 12:32:46 +1100

    v8

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import { database } from '../firebase';
 import _ from 'lodash';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import renderHTML from 'react-render-html';
 
 class App extends Component {
   constructor(props) {
@@ -11,7 +14,7 @@ class App extends Component {
       posts: {}
     };
     // bind
-    this.onInputChange = this.onInputChange.bind(this);
+    this.onHandleChange = this.onHandleChange.bind(this);
     this.onHandleSubmit = this.onHandleSubmit.bind(this);
   }
 
@@ -30,16 +33,15 @@ class App extends Component {
       return (
         <div key={key}>
           <h2>{post.title}</h2>
-          <p>{post.body}</p>
+          <p>{renderHTML(post.body)}</p>
         </div>
       );
     });
   }
 
-  onInputChange(e) {
-    this.setState({
-      [e.target.name]: e.target.value
-    });
+  onHandleChange(e) {
+    this.setState({ body: e });
+    console.log(this.state.body);
   }
 
   onHandleSubmit(e) {
@@ -65,20 +67,20 @@ class App extends Component {
               type="text"
               name="title"
               placeholder="Title"
-              onChange={this.onInputChange}
+              onChange={e => {
+                this.setState({ title: e.target.value });
+              }}
               ref="title"
               className="form-control"
             />
           </div>
           <div className="form-group">
-            <input
+            <ReactQuill
+              modules={App.modules}
+              formats={App.formats}
               value={this.state.body}
-              type="text"
-              name="body"
               placeholder="Body"
-              onChange={this.onInputChange}
-              ref="body"
-              className="form-control"
+              onChange={this.onHandleChange}
             />
           </div>
           <button className="btn btn-primary">Post</button>
@@ -89,5 +91,34 @@ class App extends Component {
     );
   }
 }
+
+App.modules = {
+  toolbar: [
+    [{ header: '1' }, { header: '2' }, { font: [] }],
+    [{ size: [] }],
+    ['bold', 'italic', 'underline', 'strike', 'blockquote'],
+    [{ list: 'ordered' }, { list: 'bullet' }],
+    ['link', 'image', 'video'],
+    ['clean'],
+    ['code-block']
+  ]
+};
+
+App.formats = [
+  'header',
+  'font',
+  'size',
+  'bold',
+  'italic',
+  'underline',
+  'strike',
+  'blockquote',
+  'list',
+  'bullet',
+  'link',
+  'image',
+  'video',
+  'code-block'
+];
 
 export default App;

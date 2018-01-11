cda11112e17fbd18314091c89ad00a960a47eb87
Ryan Dhungel <kaloraat@gmail.com>
2018-01-11 11:54:51 +1100

    done7

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,39 @@
 import React, { Component } from 'react';
 import { database } from '../firebase';
+import _ from 'lodash';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       title: '',
-      body: ''
+      body: '',
+      posts: {}
     };
     // bind
     this.onInputChange = this.onInputChange.bind(this);
     this.onHandleSubmit = this.onHandleSubmit.bind(this);
+  }
+
+  // lifecycle
+  componentDidMount() {
+    database.on('value', snapshot => {
+      this.setState({
+        posts: snapshot.val()
+      });
+    });
+  }
+
+  // render posts from firebase
+  renderPosts() {
+    return _.map(this.state.posts, (post, key) => {
+      return (
+        <div key={key}>
+          <h2>{post.title}</h2>
+          <p>{post.body}</p>
+        </div>
+      );
+    });
   }
 
   onInputChange(e) {
@@ -26,6 +49,10 @@ class App extends Component {
       body: this.state.body
     };
     database.push(post);
+    this.setState({
+      title: '',
+      body: ''
+    });
   }
 
   render() {
@@ -34,6 +61,7 @@ class App extends Component {
         <form onSubmit={this.onHandleSubmit}>
           <div className="form-group">
             <input
+              value={this.state.title}
               type="text"
               name="title"
               placeholder="Title"
@@ -44,6 +72,7 @@ class App extends Component {
           </div>
           <div className="form-group">
             <input
+              value={this.state.body}
               type="text"
               name="body"
               placeholder="Body"
@@ -54,6 +83,8 @@ class App extends Component {
           </div>
           <button className="btn btn-primary">Post</button>
         </form>
+        <br />
+        {this.renderPosts()}
       </div>
     );
   }
